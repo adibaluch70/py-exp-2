f6ebb47ce498e8c7a603ad0a5742953b0fcba266
adibaluch70 <adibaluch70@eng.rizvi.edu.in>
2026-01-16 10:35:58 +0530

    Update main.py
    python

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 # AIM: Write a python program to calculate area of rectangle.
-# Coder:
-# Date:
+# Coder:Adi A.M. Baluch
+# Date:16 JAN 2026
 
-# Write your code here
+length = float(input("Enter Length: "))
+width = float(input("Enter Width: "))
+
+print(f"Area of Rectangle = {length*width}")
